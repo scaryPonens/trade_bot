0c00769e182ce46b6a068d2f6f18adf576a39b1c
Reuben <reuben.peterpaul@stem.com>
2024-05-23 23:44:05 -0300

    Adding a donchian channel strat and implement both strategies for a given stock ticker

--- app.py
+++ app.py
@@ -1,39 +1,98 @@
+import datetime
 import os
-
+import numpy as np
+from mpl_finance import candlestick_ohlc
+import matplotlib.pyplot as plt
 import streamlit as st
+import matplotlib.dates as mpl_dates
 from dotenv import load_dotenv
 import plotly.graph_objs as go
 
-from trade_utils import signal_generator, fetch_last_60_days
+from trade_utils import signal_generator, get_donchian_channel, implement_strategy, get_signals, \
+    implement_day_trading_strategy, get_donchian_channel_signals
+import yfinance as yf
 
 st.title("Day Trading Bro")
 
-if "last_60_days" not in st.session_state:
-    st.session_state["last_60_days"] = fetch_last_60_days('STEM')
+if 'ticker' not in st.session_state:
+    st.session_state['ticker'] = 'AAPL'
+
+ticker = st.text_input('Enter a stock symbol', st.session_state['ticker'])
+
+if ticker != st.session_state['ticker'] or "last_60_days" not in st.session_state:
+    st.session_state['ticker'] = ticker
+    st.session_state["last_60_days"] = yf.download(ticker,
+                                                   start=(datetime.date.today() - datetime.timedelta(days=59)),
+                                                   end=datetime.date.today() + datetime.timedelta(days=1),
+                                                   interval='15m')
 
 candlesticks = st.session_state["last_60_days"]
-fig = go.Figure(data=[go.Candlestick(
-    x=candlesticks.index,
-    open=candlesticks['Open'],
-    high=candlesticks['High'],
-    low=candlesticks['Low'],
-    close=candlesticks['Close'],
-    name='Candlesticks',
-)])
-fig.update_layout(
-    title='STEM Candlesticks',
-    xaxis_title='',
-    yaxis_title='Price',
-    xaxis_rangeslider_visible=False,
-    width=600,
-    height=600
-)
-st.write(fig)
+candlesticks = get_signals(candlesticks)
 
-signals = ['HODL']
-for i in range(1, len(candlesticks)):
-    pair = candlesticks.iloc[i - 1:i + 1]
-    signals.append(signal_generator(pair))
-candlesticks['Signal'] = signals
+col1, col2 = st.columns(2)
+with col1:
+    st.header('Day Trading Strat')
+    st.markdown('''
+    This strategy is based on the concept of engulfing candlesticks. An engulfing candlestick is a candlestick pattern that
+    is formed when a small candlestick is followed by a larger candlestick that completely engulfs the smaller one. This
+    pattern is often seen as a sign of a potential uptrend or a bullish reversal, especially after a bearish period.
+    A bearish engulfing candlestick pattern is formed when a small bullish candlestick is followed by a larger bearish
+    candlestick that completely engulfs the smaller one. This pattern is often seen as a sign of a potential downtrend or a
+    bearish reversal, especially after a bullish period.
+    
+    A sell signal is generated when a bearish engulfing candlestick pattern is formed, while a buy signal is generated when
+    a bullish engulfing candlestick pattern is formed.
+    ''')
+    logs = implement_day_trading_strategy(candlesticks, 1000)
+    st.write(logs)
 
-st.write(candlesticks.Signal.value_counts())
+    ohlc = candlesticks[['Open', 'High', 'Low', 'Close']]
+    ohlc['date'] = candlesticks.index
+    ohlc['date'] = ohlc['date'].apply(mpl_dates.date2num)
+    cols = list(ohlc)
+    cols.insert(0, cols.pop(cols.index('date')))
+    ohlc = ohlc.loc[:, cols]
+    ohlc = ohlc.astype(float)
+    fig1, ax1 = plt.subplots(figsize=(20, 10))
+    candlestick_ohlc(ax1, ohlc.values, width=0.4, colorup='blue', colordown='grey')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Price')
+    ax1.scatter(candlesticks[candlesticks['Signal'] == 'BUY'].index, candlesticks[candlesticks['Signal'] == 'BUY'].Close,
+                color='green', s=200, marker='^', label='BUY')
+    ax1.scatter(candlesticks[candlesticks['Signal'] == 'SELL'].index, candlesticks[candlesticks['Signal'] == 'SELL'].Close,
+                color='red', s=200, marker='v', label='SELL')
+    fig1.suptitle(f'{ticker} 15m Candlesticks')
+    st.pyplot(fig1)
+
+with col2:
+    st.header('Donchian Strat')
+    st.markdown("""
+    This strategy is based on the Donchian Channel, which is a trend-following indicator that plots the highest high and
+    lowest low over a specific period of time. The Donchian Channel is typically used to identify potential buy and sell
+    signals based on the price breaking out of the channel.
+    
+    A buy signal is generated when the price breaks above the upper band of the Donchian Channel, while a sell signal is
+    generated when the price breaks below the lower band of the Donchian Channel.
+    """)
+    days_in_past = 365
+    stock = yf.download(ticker, start=(datetime.date.today() - datetime.timedelta(days=days_in_past)))
+    stock = get_donchian_channel(stock, 40, 50).dropna()
+    stock = get_donchian_channel_signals(stock)
+    logs = implement_strategy(stock, 1000)
+    st.write(logs)
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax.plot(stock[-days_in_past:].Close, label='CLOSE')
+    ax.plot(stock[-days_in_past:].dcl, color='black', linestyle='--', alpha=0.3)
+    ax.plot(stock[-days_in_past:].dcm, color='orange', label='DCM')
+    ax.plot(stock[-days_in_past:].dcu, color='black', linestyle='--', alpha=0.3, label='DCU,DCL')
+    ax.scatter(stock[stock['Signal'] == 'BUY'].index, stock[stock['Signal'] == 'BUY'].Close, color='green', s=40,
+               marker='^', label='BUY')
+    ax.scatter(stock[stock['Signal'] == 'SELL'].index, stock[stock['Signal'] == 'SELL'].Close, color='red', s=40,
+               marker='v', label='SELL')
+    ax.legend()
+    ax.set_title(f'{ticker} DONCHIAN CHANNELS 50')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Close (USD)')
+    # d.hist(np.random.normal(1, 1, size=1000))
+    st.pyplot(fig)
--- trade_utils.py
+++ trade_utils.py
@@ -1,7 +1,9 @@
 import datetime
-
+from ctypes import Union
+from termcolor import colored as cl
 import pandas as pd
 import yfinance as yf
+from pandas import Timedelta
 
 
 def _bearish_candlesticks(candle_sticks: pd.DataFrame) -> bool:
@@ -9,14 +11,14 @@ def _bearish_candlesticks(candle_sticks: pd.DataFrame) -> bool:
     This function returns True if the latest candlesticks demonstrate a bearish engulfing pattern
     ```
         Bearish Engulfing Pattern:
-            |
-         ___|___     _______
+                        |
+            |           |
+         ___|___     ___|___
         |       |   |       |
         |       |   |       |
         |___ ___|   |       |
+            |       |       |
             |       |_______|
-            |           |
-            |           |
         -----------------------
           04-01       04-02
     ```
@@ -45,14 +47,15 @@ def _bullish_candlesticks(candle_sticks: pd.DataFrame) -> bool:
     This function returns True if the latest candlesticks demonstrate a bullish engulfing pattern
     ```
         Bullish Engulfing Pattern:
-            |
-         ___|___     _______
+                        |
+                     ___|___
+         ___|___    |       |
         |       |   |       |
         |       |   |       |
         |___ ___|   |       |
             |       |_______|
-            |           |
-            |           |
+                        |
+                        |
         -----------------------
           04-01       04-02
     ```
@@ -90,12 +93,100 @@ def signal_generator(candle_sticks: pd.DataFrame) -> str:
         return 'HODL'
 
 
-def fetch_last_60_days(ticker: str) -> pd.DataFrame:
+def get_signals(data: pd.DataFrame) -> pd.DataFrame:
+    signals = ['HODL']
+    for i in range(1, len(data)):
+        pair = data.iloc[i - 1:i + 1]
+        signals.append(signal_generator(pair))
+    data['Signal'] = signals
+    return data
+
+
+def get_donchian_channel_signals(data: pd.DataFrame) -> pd.DataFrame:
+    signals = ['HODL', 'HODL', 'HODL']
+    in_position = False
+    for i in range(3, len(data)):
+        if data.High.iloc[i] == data.dcu.iloc[i] and not in_position:
+            signals.append('BUY')
+            in_position = True
+        elif data.Low.iloc[i] == data.dcl.iloc[i] and in_position:
+            signals.append('SELL')
+            in_position = False
+        else:
+            signals.append('HODL')
+
+    data['Signal'] = signals
+    return data
+
+
+def get_donchian_channel(data: pd.DataFrame, lower_length: int = 40, upper_length: int = 50) -> pd.DataFrame:
     """
-    This function fetches the last 60 days of historical data for a given stock ticker.
-    :param ticker:
-    :return:
+    This function calculates the Donchian Channel for given stock data. The Donchian Channel is a trend-following
+    indicator that plots the highest high and lowest low over a specific period of time, typically 20 days.
+
+    :param data: the stock data, expecting a yfinance pandas DataFrame
+    :param lower_length: the short period, default is 40
+    :param upper_length: the long period, default is 50
+    :return: a pandas DataFrame containing the Donchian Channel data
     """
-    today = datetime.datetime.utcnow()
-    sixty_days_ago = today - datetime.timedelta(days=60)
-    return yf.download(ticker, start=sixty_days_ago, end=today, interval='15m')
+    data['dcu'] = data['High'].rolling(window=Timedelta(days=upper_length)).max()
+    data['dcl'] = data['Low'].rolling(window=Timedelta(days=lower_length)).min()
+    data['dcm'] = (data['dcu'] + data['dcl']) / 2
+    data = data.dropna()
+    return data
+
+
+def implement_strategy(stock, investment):
+    """
+    This function implements a simple trading strategy based on the Donchian Channel. The strategy is as follows:
+    - Buy when the stock price reaches the upper band of the Donchian Channel
+    - Sell when the stock price reaches the lower band of the Donchian Channel
+
+    :param stock:  the stock data, expecting a yfinance pandas DataFrame
+    :param investment:  the initial investment amount
+    :return:  None
+    """
+    no_of_shares = 0
+    equity = investment
+    logs = []
+
+    for i in range(3, len(stock)):
+        if stock.High.iloc[i] == stock.dcu.iloc[i] and not no_of_shares:
+            no_of_shares = equity // stock['Close'].iloc[i]
+            equity -= no_of_shares * stock['Close'].iloc[i]
+            logs.append(f':green[BUY]: {no_of_shares} Shares are bought at {stock.Close.iloc[i]}USD'
+                        f' on {str(stock.index[i])[:10]}')
+        elif stock['Low'].iloc[i] == stock['dcl'].iloc[i] and no_of_shares > 0:
+            equity += no_of_shares * stock['Close'].iloc[i]
+            logs.append(f':red[SELL]: {no_of_shares} Shares are sold at {stock.Close.iloc[i]}USD'
+                        f' on {str(stock.index[i])[:10]}')
+            no_of_shares = 0
+    if no_of_shares > 0:
+        equity += no_of_shares * stock['Close'].iloc[i]
+        logs.append(f'\n*Closing position at {stock.Close.iloc[i]}USD on {str(stock.index[i])[:10]}*')
+
+    earnings = round(equity - investment, 2)
+    roi = round(earnings / investment * 100, 2)
+    logs.append(f'\n**INITIAL: {investment}USD ; EARNING: {earnings}USD ; ROI: {roi}%**')
+    return '\n\n'.join(logs)
+
+
+def implement_day_trading_strategy(stock, investment) -> str:
+    no_of_shares = 0
+    equity = investment
+    logs = []
+
+    for i in range(0, len(stock)):
+        if stock.Signal.iloc[i] == 'BUY' and not no_of_shares:
+            no_of_shares = equity // stock['Close'].iloc[i]
+            equity -= no_of_shares * stock['Close'].iloc[i]
+        elif stock.Signal.iloc[i] == 'SELL' and no_of_shares > 0:
+            equity += no_of_shares * stock['Close'].iloc[i]
+            no_of_shares = 0
+    if no_of_shares > 0:
+        equity += no_of_shares * stock['Close'].iloc[-1]
+
+    earnings = round(equity - investment, 2)
+    roi = round(earnings / investment * 100, 2)
+    logs.append(f'\n**INITIAL: {investment}USD ; EARNING: {earnings}USD ; ROI: {roi}%**')
+    return '\n\n'.join(logs)
